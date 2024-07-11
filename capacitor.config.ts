76855138f7ae32f3879fa419c357910586e6230e
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2024-07-10 20:33:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'manage_class',
+  webDir: 'www'
+};
+
+export default config;
